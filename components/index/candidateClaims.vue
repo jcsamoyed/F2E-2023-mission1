81d873699bbf75aee457b143a396289c444e1b21
jcsamoyed <aruby5223@gmail.com>
2023-11-12 01:18:09 +0800

    feat(candidate-claims): 新增候選主張區塊

---
+++ components/index/candidateClaims.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="candidate-claims">
+    <div class="container">
+      <div class="row">
+        <SectionTitle title="候選主張" subtitle="Candidate Claims" />
+        <article>
+          <p>
+            我黑新星堅信，藉由推動更完善的貓咪福利和相關政策，更是間接地投資於台灣的未來。畢竟，民眾的身心健康與工作熱情是推動經濟的核心動力。透過完善的貓咪福利政策，為台灣的 GDP 經濟帶來巨大效益。
+          </p>
+          <p>因此，我期望能在立法院內推進這些政策，確保每一隻貓咪都能得到他們應有的照顧，同時也為台灣的經濟發展助一臂之力。讓我們一同護航台灣的幸福經濟，從照顧每一隻貓咪開始。</p>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+article {
+  grid-column-start: 4;
+  grid-column-end: 13;
+  font-size: 20px;
+  line-height: 30px;
+}
+
+p {
+  margin-bottom: 30px;
+
+  &:last-child {
+    margin-bottom: unset;
+  }
+}
+
+@media (max-width:1024px) {
+  article {
+    font-size: 16px;
+    line-height: 28px;
+  }
+
+  p {
+    margin-bottom: 28px;
+
+    &:last-child {
+      margin-bottom: unset;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <IndexHeader />
+    <IndexCandidateClaims />
   </div>
 </template>
