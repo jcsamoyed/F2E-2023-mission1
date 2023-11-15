836e00b53cef141a07a187d4d1ab691824beac19
jocelyn <jocelyn@ecloudmobile.com>
2023-11-15 17:25:52 +0800

    build: 新增 head 全域設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: '黑新星 | 2024 競選官方網站',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '喵喵權益我帶頭，我為喵民力爭上游。' },
+        { name: 'theme-color', content: '#a8c400' },
+        { property: 'og:title', content: '黑新星 | 2024 競選官方網站' },
+        { property: 'og:description', content: '喵喵權益我帶頭，我為喵民力爭上游。' },
+        { property: 'og:url', content: 'https://jcsamoyed.github.io/F2E-2023-mission1' },
+      ],
+    },
+  },
   css: ['~/assets/css/main.scss'],
   vite: {
     css: {
