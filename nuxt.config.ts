539c1b66f1189e63375821e70f1b56bf52fe37af
jcsamoyed <aruby5223@gmail.com>
2023-11-18 17:49:03 +0800

    feat: 新增 og-image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
         { name: 'theme-color', content: '#a8c400' },
         { property: 'og:title', content: '黑新星 | 2024 競選官方網站' },
         { property: 'og:description', content: '喵喵權益我帶頭，我為喵民力爭上游。' },
+        { property: 'og:image', content: 'https://jcsamoyed.github.io/F2E-2023-mission1/og-image.png' },
         { property: 'og:url', content: 'https://jcsamoyed.github.io/F2E-2023-mission1' },
       ],
       link: [
